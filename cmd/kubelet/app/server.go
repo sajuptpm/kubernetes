17911ef210daee0c3b7203c684f2815d3221dee4
Darren Shepherd <darren@rancher.com>
2019-08-26 15:55:35 -0700

    Add stopCh to apiserver & context to kublet commands
    Remove SetupSignalContext call from the apiserver
    
    Signed-off-by: galal-hussein <hussein.galal.ahmed.11@gmail.com>

--- cmd/kubelet/app/server.go
+++ cmd/kubelet/app/server.go
@@ -63,7 +63,6 @@ import (
 	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 	"k8s.io/apimachinery/pkg/util/wait"
-	genericapiserver "k8s.io/apiserver/pkg/server"
 	"k8s.io/apiserver/pkg/server/healthz"
 	utilfeature "k8s.io/apiserver/pkg/util/feature"
 	clientset "k8s.io/client-go/kubernetes"
@@ -282,7 +281,6 @@ is checked every 20 seconds (also configurable with a flag).`,
 			if err := checkPermissions(ctx); err != nil {
 				logger.Error(err, "Kubelet running with insufficient permissions")
 			}
-
 			// make the kubelet's config safe for logging
 			config := kubeletServer.KubeletConfiguration.DeepCopy()
 			for k := range config.StaticPodURLHeader {
@@ -290,9 +288,6 @@ is checked every 20 seconds (also configurable with a flag).`,
 			}
 			// log the kubelet's config for inspection
 			logger.V(5).Info("KubeletConfiguration", "configuration", klog.Format(config))
-
-			// set up signal context for kubelet shutdown
-			ctx := genericapiserver.SetupSignalContext()
 
 			utilfeature.DefaultMutableFeatureGate.AddMetrics()
 			// run the kubelet
@@ -516,7 +511,8 @@ func UnsecuredDependencies(ctx context.Context, s *options.KubeletServer, featur
 		OSInterface:         kubecontainer.RealOS{},
 		VolumePlugins:       plugins,
 		DynamicPluginProber: GetDynamicPluginProber(s.VolumePluginDir, pluginRunner),
-		TLSOptions:          tlsOptions}, nil
+		TLSOptions:          tlsOptions,
+	}, nil
 }
 
 // Run runs the specified KubeletServer with the given Dependencies. This should never exit.
@@ -883,7 +879,6 @@ func run(ctx context.Context, s *options.KubeletServer, kubeDeps *kubelet.Depend
 			kubeDeps.Recorder,
 			kubeDeps.KubeClient,
 		)
-
 		if err != nil {
 			return err
 		}
@@ -1284,7 +1279,8 @@ func createAndInitKubelet(
 	kubeDeps *kubelet.Dependencies,
 	hostname string,
 	nodeName types.NodeName,
-	nodeIPs []net.IP) (k kubelet.Bootstrap, err error) {
+	nodeIPs []net.IP,
+) (k kubelet.Bootstrap, err error) {
 	// TODO: block until all sources have delivered at least one update to the channel, or break the sync loop
 	// up into "per source" synchronizations
 
